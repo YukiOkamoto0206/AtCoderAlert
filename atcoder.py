f9f080e389c45dffbdb93292107f685dfbc2a275
Yuki Okamoto <okamotoyu@crunchtimer.jp>
2021-03-12 19:12:40 +0900

    add: slack連携

--- atcoder.py
+++ atcoder.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import slackweb
+
+SLACK_URL = 'https://hooks.slack.com/services/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'
 
 # ダウンロード
 url = 'https://atcoder.jp/contests'
@@ -12,5 +15,8 @@ soup = BeautifulSoup(response.content, "html.parser")
 # 予定されたコンテスト
 elems = soup.select("#contest-table-upcoming a")
 
+slack = slackweb.Slack(SLACK_URL)
+
+
 for elem in elems:
-    print(elem.get_text())
+    print(slack.notify(text=elem.get_text()))
